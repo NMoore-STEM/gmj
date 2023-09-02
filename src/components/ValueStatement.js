22d54b427bd29ebeb747ecf890f3d2235e69958d
NMoore-STEM <66744927+NMoore-STEM@users.noreply.github.com>
2023-09-02 15:07:28 -0600

    gmj page Added files for individual page content (empty now). Added style for value statements (h4). Commented out script that will be used to randomly generate different value statements on each page load. Will return to this later and add functionality, for now, using a placeholder.

--- src/components/ValueStatement.js
+++ src/components/ValueStatement.js
@@ -1,10 +1,32 @@
 import { Component } from 'react';
 
+// const valueArray = [
+//     "YOU DESERVE\nTHE BEST.\nNOTHING LESS",
+//     "NEVER SETTLE\nFOR ANYTHING\nLESS...",
+//     "SPEND MONEY\nTO\nMAKE MONEY",
+//     "DON'T REGRET\nPASSING THIS\nMOMENT BY",
+//     "OWN JEWELRY\nAS UNIQUE\nAS YOU ARE",
+//     "IT MAY NOT\nBE HERE\nTOMORROW..."
+// ]
+
+// const getrandomvalue = () => {
+//     return valueArray[math.floor(math.random() * valueArray.length)];
+//   };
+
+// function valueblock() {
+//     const [valuetitle, setvaluetitle] = usestate(getrandomvalue());
+//     const randomvaluetitle = getrandomvalue();
+//     setvaluetitle(randomvaluetitle);
+// }
+
 class ValueStatement extends Component {
     render() {
         return (
-            <div className="value_frame">
-                <div className="value_statement"></div>
+            <div className="value_frame collection_frame">
+                <div className="value_statement">
+                    {/* <h3>{valuetitle}</h3> */}
+                    <h4>YOU DESERVE<br />THE BEST.<br />NOTHING LESS</h4>
+                </div>
             </div>
         )
     }
