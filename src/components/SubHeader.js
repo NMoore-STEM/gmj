38cc32da81efacde7f8ae251671a86117a2975c0
NMoore-STEM <66744927+NMoore-STEM@users.noreply.github.com>
2023-09-05 22:03:14 -0600

    gmj page Created separate component for product cards used specifically for the necklace page. Tried to generalize the product card as the format will be the same, but because I am importing specific JSON values, there will need to be specific card components for each product type.

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom'
 class SubHeader extends Component{
     render() {
         return (
-            <nav>
-                <Link to='sale' className="nav_button">SALE ITEMS</Link>
-                <Link to='necklaces' className="nav_button">Necklaces</Link>
-                <Link to='bracelets' className="nav_button">Bracelets</Link>
-                <Link to='earrings' className="nav_button">Earrings</Link>
-                <Link to='charms' className="nav_button">Charms</Link>
+            <nav className="sub_header">
+                <Link to='sale' className="sub_nav_button">SALE ITEMS</Link>
+                <Link to='necklaces' className="sub_nav_button">Necklaces</Link>
+                <Link to='bracelets' className="sub_nav_button">Bracelets</Link>
+                <Link to='earrings' className="sub_nav_button">Earrings</Link>
+                <Link to='charms' className="sub_nav_button">Charms</Link>
             </nav>
         )
     }
