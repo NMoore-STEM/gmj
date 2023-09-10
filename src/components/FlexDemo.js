53b2296128c79c55f085f084657eedca59e309f2
NMoore-STEM <66744927+NMoore-STEM@users.noreply.github.com>
2023-09-09 18:31:28 -0600

    gmj page Created a test component to experiment with pure CSS for store page menu item effect. It worked! No need for complicated hover states, useStates, and scripts to achieve deisred result. CSS route is more than likely more performant as well.

---
+++ src/components/FlexDemo.js
@@ -0,0 +1,31 @@
+function FlexDemo() {
+    return (
+        <>
+            <div className="store_menu_item_area_d">
+                <div className="store_menu_item_d">
+                    {/* <a href="/" style={{backgroundColor:"red",outline:"2px solid black",outlineOffset:"-2px"}}></a> */}
+                    <div></div>
+                </div>
+                <div className="store_menu_item_d">
+                    {/* <a href="/" style={{backgroundColor:"red",outline:"2px solid black",outlineOffset:"-2px"}}></a> */}
+                    <div></div>
+                </div>
+                <div className="store_menu_item_d">
+                    {/* <a href="/" style={{backgroundColor:"red",outline:"2px solid black",outlineOffset:"-2px"}}></a> */}
+                    <div></div>
+                </div>
+                <div className="store_menu_item_d">
+                    {/* <a href="/" style={{backgroundColor:"red",outline:"2px solid black",outlineOffset:"-2px"}}></a> */}
+                    <div></div>
+                </div>
+                <div className="store_menu_item_d">
+                    {/* <a href="/" style={{backgroundColor:"red",outline:"2px solid black",outlineOffset:"-2px"}}></a> */}
+                    <div></div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default FlexDemo
