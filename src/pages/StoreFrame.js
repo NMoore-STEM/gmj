17ce7490b6eee33cebb6afd5967774d06e4df6db
NMoore-STEM <66744927+NMoore-STEM@users.noreply.github.com>
2023-09-12 21:50:04 -0600

    gmj page Trying to get scroll to top to work when clicking custom sales pitch button. Could not obtain functionality at this point, will return to later.

--- src/pages/StoreFrame.js
+++ src/pages/StoreFrame.js
@@ -1,10 +1,12 @@
 import SubHeader from '../components/SubHeader';
 import CustomPitch from '../components/CustomPitch';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '../components/ScrollToTop';
 
 export default function StoreFrame() {
     return (
         <>
+            <ScrollToTop />
             <SubHeader />
             <Outlet />
             <CustomPitch />
